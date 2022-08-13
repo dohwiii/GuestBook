f6d87ca6755c84dfeb64030908bb06898225ad1b
dohwiii <ehgnl97722@gmail.com>
2022-08-13 18:49:27 +0900

    origin

---
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,51 @@
+package org.zerock.guestbook.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor //의존성 자동 주입
+public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository; //반드시 final로 선언
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+        log.info("DTO-----------------------");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+    @Override
+    public PageResultDTO<Guestbook, GuestbookDTO> getList(PageRequestDTO requestDTO)
+    {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable); //findAll()에 Pageable 타입의 파라미터로 전달하면
+        // 페이징 처리에 관련된 쿼리들을 실행하고, 이 결과들을 이용해서 리턴 타입으로 지정된 Page<엔티티 타입> 객체로 저장한다.
+
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity)); //엔티티 객체들을 DTO로 변환한다.
+
+        return new PageResultDTO<>(result, fn); //<DTO, EN>
+
+    }
+
+}
