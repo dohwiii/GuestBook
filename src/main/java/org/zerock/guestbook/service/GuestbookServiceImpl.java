a093c56c4cff84f779120d4d18f85481158f0b56
dohwiii <ehgnl97722@gmail.com>
2022-08-20 17:04:43 +0900

    Board modify / delete / search

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,7 @@
 package org.zerock.guestbook.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
 import java.util.Optional;
@@ -40,7 +43,9 @@ public class GuestbookServiceImpl implements GuestbookService {
     {
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = repository.findAll(pageable); //findAll()에 Pageable 타입의 파라미터로 전달하면
+        BooleanBuilder booleanBuilder = getSearch(requestDTO); //검색 조건 처리
+
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable); //findAll()에 Pageable 타입의 파라미터로 전달하면
         // 페이징 처리에 관련된 쿼리들을 실행하고, 이 결과들을 이용해서 리턴 타입으로 지정된 Page<엔티티 타입> 객체로 저장한다.
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity)); //엔티티 객체들을 DTO로 변환한다.
@@ -55,6 +60,61 @@ public class GuestbookServiceImpl implements GuestbookService {
         Optional<Guestbook> result = repository.findById(gno);
 
         return result.isPresent() ? entityToDto(result.get()) : null;
+    }
+
+    @Override
+    public void remove(Long gno) {
+
+        repository.deleteById(gno);
+    }
+
+    @Override
+    public void modify(GuestbookDTO dto) {
+
+        Optional<Guestbook> result = repository.findById(dto.getGno());
+
+        if (result.isPresent())
+        {
+
+            Guestbook entity = result.get();
+
+            entity.changeTitle(dto.getTitle());
+            entity.changeContent(dto.getContent());
+
+            repository.save(entity);
+
+        }
+    }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+
+        String type = requestDTO.getType();
+        String keyword = requestDTO.getKeyword();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L); //gno>0 조건만 생성
+        booleanBuilder.and(expression);
+
+        if (type == null || type.trim().length() == 0) { //검색 조건이 없는 경우
+            return booleanBuilder;
+
+        }
+        //검색 조건 작성하기
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+        if (type.contains("t")) {
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if (type.contains("c")) {
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) {
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+        //모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
     }
 
 }
--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.dto.PageResultDTO;
@@ -42,6 +43,29 @@ public class GuestbookServiceTests {
             System.out.println(guestbookDTO);
 
         }
+        System.out.println("=================================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+    @Test
+    public void testSearch()
+    {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc") //검색 조건 t, c, w, tc, tcw
+                .keyword("한글") //검색 키워드
+                .build();
+        PageResultDTO<Guestbook, GuestbookDTO> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("------------------------------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
         System.out.println("=================================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
